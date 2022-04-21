6d531b1187ab21262b2393bf473175826cd38cdb
Sarti <riccardo.sartor04@gmail.com>
2022-04-21 22:27:55 +0200

    File di base
    ho aggiunto il porcoddio

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+  if (msg.content.includes('ping')) {
+    msg.reply('pong');
+
+  } else if (msg.content.startsWith('!kick')) {
+    if (msg.mentions.users.size) {
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    } else {
+      msg.reply('Please tag a valid user!');
+    }
+  }
+});
