bb9404b29361207708aa3d45447e70e013f0ee1d
Sarti <riccardo.sartor04@gmail.com>
2022-04-21 22:32:03 +0200

    Update index.js
    Go sistemà un poche de robe

--- index.js
+++ index.js
@@ -12,13 +12,5 @@ bot.on('ready', () => {
 bot.on('message', msg => {
   if (msg.content.includes('ping')) {
     msg.reply('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
   }
 });
