4798465fcbd17181fe17e0a707d9e878d225f0cd
Blvk <naciri.pentest@gmail.com>
2022-04-25 00:36:02 +0200

    remove bambino with name

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const helpMessage = `
     Bot creato da <@428651109712789524> e <@764083440466657280>
     I comandi NON sono case-sensitive  :wink:
 
-    **v1.0.1**
+    **v1.0.2**
 
     `
 
@@ -125,7 +125,16 @@ client.on('messageCreate', async message => {
 
                 message.reply("Bambino rimosso");
             } else {
-                message.reply("Non trovo il moccioso");
+                if(nomi.has(bambinoID)){
+                    bambini.forEach(function(item, index){
+                        if(item == nomi.get(bambinoID)){
+                            bambini.splice(index, 1);
+                        }
+                    });
+                    message.reply("Bambino rimosso");
+                } else {
+                    message.reply("Non trovo il moccioso");
+                }
             }
 
         } else if(message.content.toLowerCase().match(cmd + "flush")){               //Flush Bambini
